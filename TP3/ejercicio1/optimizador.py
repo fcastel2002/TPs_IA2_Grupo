cb0dec4025e923befab2ee9aaada8643a26c002e
fcastel2002 <146264604+fcastel2002@users.noreply.github.com>
2025-05-31 18:20:20 -0300

    mejora en optimizador ej 1

--- TP3/ejercicio1/optimizador.py
+++ TP3/ejercicio1/optimizador.py
@@ -1,11 +1,12 @@
 from parser import cargar_datos
 import numpy as np
 from red_neuronal import RedNeuronal, linear, linear_derivative, leaky_relu, leaky_relu_derivative
+import matplotlib.pyplot as plt
 
 puntos = cargar_datos()
 X = puntos[:, 0].reshape(-1, 1)
 Y = puntos[:, 1].reshape(-1, 1)
-grado = 2
+grado = 1
 X_poly = np.hstack([X ** i for i in range(1, grado + 1)])
 
 mejor_loss = float('inf')
@@ -13,15 +14,15 @@ mejor_lr = None
 mejor_neuronas = None
 
 # Random search
-n_iter = 20  # Número de combinaciones aleatorias a probar
+n_iter = 60  # Número de combinaciones aleatorias a probar
 np.random.seed(42)
 for _ in range(n_iter):
     lr = 10 ** np.random.uniform(-4, -1)  # learning rate entre 0.0001 y 0.1
     neuronas = np.random.randint(1, 10)   # entre 1 y 10 neuronas ocultas
     modelo = RedNeuronal(
-        capas_entrada=grado,
-        capas_ocultas=neuronas,
-        capas_salida=1,
+        neuronas_entrada=grado,
+        neuronas_ocultas=neuronas,
+        neuronas_salida=1,
         learning_rate=lr,
         activacion_oculta=leaky_relu,
         derivada_oculta=leaky_relu_derivative,
@@ -39,3 +40,27 @@ for _ in range(n_iter):
 print(f"Mejor loss: {mejor_loss:.4f}")
 print(f"Mejor learning rate: {mejor_lr}")
 print(f"Mejor número de neuronas ocultas: {mejor_neuronas}")
+
+# Crear y entrenar la mejor red neuronal encontrada
+mejor_modelo = RedNeuronal(
+    neuronas_entrada=grado,
+    neuronas_ocultas=mejor_neuronas,
+    neuronas_salida=1,
+    learning_rate=mejor_lr,
+    activacion_oculta=leaky_relu,
+    derivada_oculta=leaky_relu_derivative,
+    activacion_salida=linear,
+    derivada_salida=linear_derivative,
+    alpha=0.02
+)
+mejor_modelo.train(X_poly, Y, epochs=1500, verbose=False)
+y_pred = mejor_modelo.predict(X_poly)
+
+# Mostrar el gráfico
+plt.scatter(X, Y, s=10, label='Datos Originales', color='blue')
+plt.scatter(X, y_pred, s=10, label='Predicciones', color='red')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Mejor red neuronal encontrada en 60 iteraciones')
+plt.legend("lr = {:.4f}, neuronas = {}".format(mejor_lr, mejor_neuronas))
+plt.show()
