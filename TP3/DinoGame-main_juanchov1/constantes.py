f8ebec6e4f752a0334b24b0ab7fa978c6e07b438
juanchoq <juanchoquiroga007@gmail.com>
2025-06-05 10:06:26 -0300

    versiones_dinosaurio_juancho

---
+++ TP3/DinoGame-main_juanchov1/constantes.py
@@ -0,0 +1,10 @@
+# Global Constants
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1100
+SCREEN_SPAWN_POSITION = (100, 100)
+FONDO_BLANCO = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+MAX_OBSTACLE_WIDTH  = 105     # ancho máximo entre Small y Large Cactus
+MAX_OBSTACLE_HEIGHT = 95      # alto máximo entre Cactus y Bird
+MAX_GAME_SPEED      = 70     # velocidad tope que consideramos
